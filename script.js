a11bf8f8b5a7484ace3abe41908cd126e27ac461
YuttaStore <metubing21@gmail.com>
2024-08-06 21:34:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
                    
                    
                    
-*INVOICE BILL from* yuttastore.com
+*INVOICE BILL from* yutta.store
                  
 Time: ${timestamp} 
 ------------------------------------------------------
@@ -44,7 +44,7 @@ Please make the payment to complete your order. Thank you!
 
         // Encode the message for WhatsApp
         const encodedMessage = encodeURIComponent(invoiceMessage);
-        const whatsappUrl = `https://wa.me/8414075796?text=${encodedMessage}`;
+        const whatsappUrl = `https://wa.me/8258991325?text=${encodedMessage}`;
 
         // Redirect to WhatsApp
         window.open(whatsappUrl, '_blank');
